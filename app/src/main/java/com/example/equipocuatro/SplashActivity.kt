5ff5b757243ad968f2de392e95d0bb85329e9d1a
bryan_cr <cordoba.brayan@correounivalle.edu.co>
2026-05-10 14:39:55 -0500

    dialogos: EditarReto y EliminarReto

--- app/src/main/java/com/example/equipocuatro/SplashActivity.kt
+++ app/src/main/java/com/example/equipocuatro/SplashActivity.kt
@@ -1,6 +1,9 @@
 package com.example.equipocuatro
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/example/equipocuatro/dialogos/EliminarReto.kt
@@ -0,0 +1,41 @@
+package com.example.equipocuatro.dialogos
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.example.equipocuatro.R
+
+class EliminarReto {
+    companion object {
+        fun showDialogoEliminarReto(
+            context: Context,
+            reto: String = "",
+            onEliminar: () -> Unit = {}
+        ) {
+            val view = LayoutInflater.from(context).inflate(R.layout.dialog_eliminar_reto, null)
+            val tvRetoDeleteDescription = view.findViewById<TextView>(R.id.tvRetoDeleteDescription)
+            val btnNoDelete = view.findViewById<Button>(R.id.btnNoDelete)
+            val btnSiDelete = view.findViewById<Button>(R.id.btnSiDelete)
+
+            val alertDialog = AlertDialog.Builder(context).create()
+            alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+            alertDialog.setCancelable(false)
+            alertDialog.setView(view)
+
+            tvRetoDeleteDescription.text = reto
+
+            btnNoDelete.setOnClickListener {
+                alertDialog.dismiss()
+            }
+
+            btnSiDelete.setOnClickListener {
+                onEliminar()
+                alertDialog.dismiss()
+            }
+
+            alertDialog.show()
+        }
+    }
+}
