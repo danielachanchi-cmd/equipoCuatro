fda7d76862cde8d943b04f49bb5b5de86166fdf8
Daniel-F-Arias <daniel.felipe.arias@correounivalle.edu.co>
2026-05-14 17:17:01 -0500

    agregar carpeta y archivo nav_graph.xml
    - Se creó la carpeta `navigation/` y el archivo `nav_graph.xml`.
    - Se configuró el Navigation Component con el grafo inicial.

--- app/src/main/java/com/example/equipocuatro/ui/fragments/fragment_retos.kt
+++ app/src/main/java/com/example/equipocuatro/ui/fragments/fragment_retos.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.equipocuatro.R
 import com.example.equipocuatro.databinding.FragmentRetosBinding
 import com.example.equipocuatro.ui.dialogs.AgregarReto
 
@@ -25,12 +27,19 @@ class fragment_retos : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         agregarReto()
+        volver()
     }
 
 
     private fun agregarReto(){
         binding.btnAgregar.setOnClickListener {
             AgregarReto.Companion.showDialgoAgregarReto(binding.root.context)
+        }
+    }
+
+    private fun volver(){
+        binding.toolbarRetos.btnBack.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment_retos_to_home_principal24)
         }
     }
 }
